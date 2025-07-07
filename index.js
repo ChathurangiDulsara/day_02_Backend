51e67fe87b1666cfac820dad6e5ffd8106e42df5
Chathurangi <you@example.com>
2025-07-07 14:16:43 +0530

    ✅ change secret key

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
         console.log(token)
 
         if(token!= null){
-            jwt.verify(token,"cbc-secret-key-7973",(error,decoded)=>{
+            jwt.verify(token,proces.env.SECRET,(error,decoded)=>{
                 if(!error){
                     req.user = decoded 
                 }
