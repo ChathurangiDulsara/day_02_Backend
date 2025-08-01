fd0e759ca970c4c0249b70630099accf8fe38b33
Chathurangi <you@example.com>
2025-08-02 00:11:38 +0530

    change userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
-import {createUser,loginUser,getUser} from "../Controllers/usersController.js";
+
+import {createUser,loginUser,getUser, googleLogin} from "../Controllers/usersController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/",getUser);
-
+userRouter.post("/google",googleLogin);
 userRouter.post("/",createUser);
+userRouter.post("/login",loginUser);
 
-userRouter.post("/login",loginUser)
 
 export default userRouter;
 
